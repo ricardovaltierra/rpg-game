a141bb415d2c8f07d360fc480c52e09ec909a66b
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-07-16 18:49:00 -0500

    added credits text

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,8 +8,16 @@ export default class CreditsScene extends Phaser.Scene {
   }
 
   create () {
-    this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.title = this.add.text(0, 0, 'Little Red Riding Hood', { fontSize: '32px', fontStyle: 'bold', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Ricardo Valtierra', { fontSize: '26px', fill: '#fff' });
+    this.capstone = this.add.text(0, 0, 'Capstone project with Phaser 3 FW', { fontSize: '26px', fill: '#fff' });
+    this.webpack = this.add.text(0, 0, 'ES6 and Webpack 3', { fontSize: '26px', fill: '#fff' });
+    this.description = this.add.text(0, 0, 'Aknowledgments:', { fontSize: '26px', fontStyle: 'bold', fill: '#fff' });
+    this.microverse = this.add.text(0, 0, 'Microverse', { fontSize: '20px', fill: '#fff' });
+    this.phaser = this.add.text(0, 0, 'Phaser 3', { fontSize: '20px', fill: '#fff' });
+    this.gamedev = this.add.text(0, 0, 'GameDev Academy', { fontSize: '20px', fill: '#fff' });
+    this.gameart = this.add.text(0, 0, 'Open Game Art', { fontSize: '20px', fill: '#fff' });
+    this.imageart = this.add.text(0, 0, 'deviantart - slimmmeiske2', { fontSize: '20px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
     Phaser.Display.Align.In.Center(
--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -12,13 +12,13 @@ export default class OptionsScene extends Phaser.Scene {
 
     this.model = this.sys.game.globals.model;
 
-    this.text = this.add.text(320, 200, 'Options', { fontSize: 40 });
+    this.text = this.add.text(320, 200, 'Options', { fontSize: 35 });
 
-    this.musicButton = this.add.image(80, 350, 'checkedBox');
-    this.musicText = this.add.text(130, 340, 'Music Enabled', { fontSize: 24 });
+    this.musicButton = this.add.image(300, 310, 'checkedBox');
+    this.musicText = this.add.text(350, 300, 'Music Enabled', { fontSize: 20 });
 
-    this.soundButton = this.add.image(500, 350, 'checkedBox');
-    this.soundText = this.add.text(550, 340, 'Sound Enabled', { fontSize: 24 });
+    this.soundButton = this.add.image(300, 390, 'checkedBox');
+    this.soundText = this.add.text(350, 380, 'Sound Enabled', { fontSize: 20 });
 
     this.musicButton.setInteractive();
     this.soundButton.setInteractive();
@@ -40,11 +40,11 @@ export default class OptionsScene extends Phaser.Scene {
 
   updateAudio() {
     if (this.model.musicOn === false) {
-      this.musicButton.setTexture('box').scale = 0.5;
+      this.musicButton.setTexture('box').scale = 0.3;
       this.sys.game.globals.bgMusic.stop();
       this.model.bgMusicPlaying = false;
     } else {
-      this.musicButton.setTexture('checkedBox').scale = 0.5;
+      this.musicButton.setTexture('checkedBox').scale = 0.3;
       if (this.model.bgMusicPlaying === false) {
         this.sys.game.globals.bgMusic.play();
         this.model.bgMusicPlaying = true;
@@ -52,9 +52,9 @@ export default class OptionsScene extends Phaser.Scene {
     }
   
     if (this.model.soundOn === false) {
-      this.soundButton.setTexture('box').scale = 0.5;
+      this.soundButton.setTexture('box').scale = 0.3;
     } else {
-      this.soundButton.setTexture('checkedBox').scale = 0.5;
+      this.soundButton.setTexture('checkedBox').scale = 0.3;
     }
   }
 };
